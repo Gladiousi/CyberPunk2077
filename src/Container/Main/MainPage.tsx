a97a20936cb73d9ce817412cf2d34a9efbaef0d3
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-15 12:58:53 +0300

    first GL commit

--- src/Container/Main/MainPage.tsx
+++ src/Container/Main/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './MainPage.module.css';
 import Footer from '../../Components/Footer/Footer';
+import Buy from '../../Components/Buy/Buy';
 
 function MainPage () {
     return(
         <>
+            <Buy />
             <Footer />
         </>
     )
