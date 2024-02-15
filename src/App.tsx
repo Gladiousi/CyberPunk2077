88a94c4bd8663acc35740defcc7daf4b572fb493
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-15 14:24:18 +0300

    update architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Button, ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { theme as chakraTheme } from '@chakra-ui/react';
-import MainPage from "./Container/Main/MainPage";
+import MainPage from "./Container/MainPage";
 
 const theme = extendTheme({
   ...chakraTheme,
@@ -9,7 +9,6 @@ const theme = extendTheme({
 export function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="blue" bg="black">Пример кнопки Chakra UI</Button>
       <MainPage />
     </ChakraProvider>
   );
