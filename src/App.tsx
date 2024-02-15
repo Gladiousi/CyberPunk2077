ff26c0df02166ba0cbf417920f1fd5ca32ff9280
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-15 15:32:27 +0300

    \

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import MainPage from "./Container/MainPage";
-import theme from './theme/Theme';
 
 export function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <MainPage />
-    </ChakraProvider>
-    // TESt
+    <MainPage />
   );
 }
 
