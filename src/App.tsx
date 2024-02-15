c81dd2b592e3ffbc3fcd9fc053ccabfd7149af5f
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-15 13:27:58 +0300

    add Chakra UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Button, ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { theme as chakraTheme } from '@chakra-ui/react';
 import MainPage from "./Container/Main/MainPage";
 
-function App() {
+const theme = extendTheme({
+  ...chakraTheme,
+});
+
+export function App() {
   return (
-    <>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="blue" bg="black">Пример кнопки Chakra UI</Button>
       <MainPage />
-    </>
-  )
+    </ChakraProvider>
+  );
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  esbuild: {
+    jsxFactory: 'jsx',
+    jsxInject: `import { jsx } from '@emotion/react';`,
+  },
+  resolve: {
+    alias: {
+      '@emotion/react': resolve(__dirname, './node_modules/@emotion/react'),
+      '@emotion/styled': resolve(__dirname, './node_modules/@emotion/styled'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@chakra-ui/icons'],
+  },
+});
