87850b85632e3d5b692f1d9832fb215037e89210
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-15 12:51:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainPage from "./Container/Main/MainPage";
+
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/Container/Main/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './MainPage.module.css';
+import Footer from '../../Components/Footer/Footer';
+
+function MainPage () {
+    return(
+        <>
+            <Footer />
+        </>
+    )
+}
+
+export default MainPage;
