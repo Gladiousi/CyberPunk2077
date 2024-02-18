ab032fce6005df590fc37b23b0b10595fd697814
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-18 14:36:54 +0300

    start work

--- src/Container/MainPage.tsx
+++ src/Container/MainPage.tsx
@@ -1,12 +1,9 @@
 import { FC } from 'react';
-import Buy from '../Components/Buy/Buy';
-import Footer from '../Components/Footer/Footer';
 
-const MainPage: FC = () =>  {
-    return(
+const MainPage: FC = () => {
+    return (
         <>
-            <Buy />
-            <Footer />
+
         </>
     )
 }
