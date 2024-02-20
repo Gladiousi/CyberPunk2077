6f48747e7aa0faa45f7307daa6a4acb7835e6fe8
diplodog1980 <daniilmarakulin@yandex.ru>
2024-02-20 21:20:08 +0300

    Slyder

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,7 +1,32 @@
-export const Footer = () => {
+import { Box } from "@chakra-ui/react"
+import React, { useEffect, useState } from "react";
+export const Hero = () => {
+   
+        const [currentIndex, setCurrentIndex] = useState(0);
+      
+        const images = [
+            "./public/1.webp",
+            "./public/2.webp",
+            "./public/3.webp"
+        ];
+        useEffect(() => {
+          const intervalId = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+          }, 5000);
+      
+          return () => clearInterval(intervalId);
+        }, [images]);
+
     return (
-        <>
-            
+        <>      
+            <Box
+                w='100dvw' h='100dvh'
+                bgImage={`url(${images[currentIndex]})`}
+                bgSize="cover"
+                bgPosition="center"
+            >
+            </Box>
+
         </>
     )
-}
+      } 
--- src/Container/MainPage.tsx
+++ src/Container/MainPage.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
-
 const MainPage: FC = () => {
     return (
-        <>
-        </>
+       
     )
 }
 
