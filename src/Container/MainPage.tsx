5fd8c23e2066ea0dc5e16bd438ab5d4b85e8bab3
abramovdaniil426475@gmail.com <146266628+Gladiousi@users.noreply.github.com>
2024-02-25 22:22:30 +0300

    test build

--- src/Container/MainPage.tsx
+++ src/Container/MainPage.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
+import { Hero } from '../Components/Hero/Hero';
+import { Footer } from '../Components/Footer/Footer';
 const MainPage: FC = () => {
     return (
-       <></>
+        <>
+            <Hero />
+            <Footer />
+        </>
     )
 }
 
